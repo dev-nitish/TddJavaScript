22370ad9954d13063c6d8f27577ba10eb199308b
eceNitish <nitish6776@hotmail.com>
2020-06-14 10:57:52 +0530

    Added more test cases

--- findLeap.test.js
+++ findLeap.test.js
@@ -5,4 +5,7 @@ const checkLeap =  require("./findLeap");
 test("checking Leap year", () => {
     expect(checkLeap(1999)).toBe(false);
     expect(checkLeap(2100)).toBe(false);
+    expect(checkLeap(2008)).toBe(true);
+    expect(checkLeap(1700)).toBe(false);
+    expect(checkLeap(1900)).toBe(false);
 })
