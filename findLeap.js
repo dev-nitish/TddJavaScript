55877ca8e9a343e53ac6ca3acff6379c308b59e1
eceNitish <nitish6776@hotmail.com>
2020-06-14 11:35:49 +0530

    changed nested blocks into conditional ternary operator

--- findLeap.js
+++ findLeap.js
@@ -1,20 +1,5 @@
 function checkLeap(year) {
-    if(!(year % 4)){
-        if(!(year % 100)) {
-            if(!(year % 400)) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else {
-            return true;
-        }
-    }
-    else {
-        return false;
-    }
+    return(!(year % 4) ? (!(year % 100) ? (!(year % 400) ? true : false) : true) : false);
 };
 
 module.exports = checkLeap;
