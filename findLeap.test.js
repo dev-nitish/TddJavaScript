00277863d61e07090a3b787e3356d0b1c26cbc4b
eceNitish <nitish6776@hotmail.com>
2020-06-14 11:48:52 +0530

    Updated test script

--- findLeap.test.js
+++ findLeap.test.js
@@ -1,17 +1,19 @@
 //console.log("Running test");
 
-const checkLeap =  require("./findLeap");
+const checkLeap = require("./findLeap");
 
-test("checking Leap year", () => {
-    expect(checkLeap(1999)).toBe(false);
-    expect(checkLeap(2100)).toBe(false);
-    expect(checkLeap(2008)).toBe(true);
-    expect(checkLeap(1700)).toBe(false);
-    expect(checkLeap(1900)).toBe(false);
-    expect(checkLeap(2000)).toBe(true);
-    expect(checkLeap(2012)).toBe(true);
-    expect(checkLeap(2016)).toBe(true);
-    expect(checkLeap(2017)).toBe(false);
-    expect(checkLeap(2018)).toBe(false);
-    expect(checkLeap(2019)).toBe(false);
+describe("testing leap years", () => {
+    test("checking Leap year", () => {
+        expect(checkLeap(1999)).toBe(false);
+        expect(checkLeap(2100)).toBe(false);
+        expect(checkLeap(2008)).toBe(true);
+        expect(checkLeap(1700)).toBe(false);
+        expect(checkLeap(1900)).toBe(false);
+        expect(checkLeap(2000)).toBe(true);
+        expect(checkLeap(2012)).toBe(true);
+        expect(checkLeap(2016)).toBeTruthy;
+        expect(checkLeap(2017)).toBeFalsy;
+        expect(checkLeap(2018)).toBeFalsy;
+        expect(checkLeap(2019)).toBeFalsy;
+    })
 })
