b3904e081e013eb0167809e79399fedff15f55c2
eceNitish <nitish6776@hotmail.com>
2020-06-14 11:16:08 +0530

    Multiple test cases added

--- findLeap.test.js
+++ findLeap.test.js
@@ -9,4 +9,9 @@ test("checking Leap year", () => {
     expect(checkLeap(1700)).toBe(false);
     expect(checkLeap(1900)).toBe(false);
     expect(checkLeap(2000)).toBe(true);
+    expect(checkLeap(2012)).toBe(true);
+    expect(checkLeap(2016)).toBe(true);
+    expect(checkLeap(2017)).toBe(false);
+    expect(checkLeap(2018)).toBe(false);
+    expect(checkLeap(2019)).toBe(false);
 })
